7303605dd93dbe3f78701728d129dba125e6d7e7
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2013-05-11 19:26:37 +0200

    remove acra from application

--- textfairy/src/com/renard/ocr/MyApplication.java
+++ textfairy/src/com/renard/ocr/MyApplication.java
@@ -2,14 +2,12 @@ package com.renard.ocr;
 
 import java.lang.reflect.Field;
 
-import org.acra.annotation.ReportsCrashes;
-
 import android.app.Application;
 import android.view.ViewConfiguration;
 
 import com.renard.util.PreferencesUtils;
 
-@ReportsCrashes(formKey = "dDZFZXFpU1NocWUwZ0x0aURsVUhNSXc6MQ", socketTimeout = 10000)
+//@ReportsCrashes(formKey = "dDZFZXFpU1NocWUwZ0x0aURsVUhNSXc6MQ", socketTimeout = 10000)
 public class MyApplication extends Application {
 
 	public void onCreate() {
