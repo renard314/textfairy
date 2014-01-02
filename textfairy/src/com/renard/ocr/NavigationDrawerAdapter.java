e51e583f7f3e3bb5076300e57eaee4b6d4dd893b
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2014-01-02 17:34:51 +0100

    remove dummy entries from navigation drawer. add icon with padding

--- textfairy/src/com/renard/ocr/NavigationDrawerAdapter.java
+++ textfairy/src/com/renard/ocr/NavigationDrawerAdapter.java
@@ -21,7 +21,7 @@ public class NavigationDrawerAdapter extends BaseAdapter implements ListAdapter 
 
     @Override
     public int getCount() {
-        return 5;
+        return 3;
     }
 
     @Override
