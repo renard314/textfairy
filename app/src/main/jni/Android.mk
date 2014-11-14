cd44a8fb89f7de7b30911a456c5b7c1a770c4775
Renard Wellnitz <renard.wellnitz@googlemail.com>
2014-11-14 22:04:11 +0100

    remove tesseract and leptonica as it is included by teas-two. moved to new gradle android folder structure

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,22 @@
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+HOCR2PDF_PATH := $(LOCAL_PATH)/../../../../hocr2pdf/src
+HOCR2PDF_JNI_PATH := $(LOCAL_PATH)/hocr2pdf
+
+IMAGE_PROCESSING_PATH := $(LOCAL_PATH)/../../../../OCRTest/src
+IMAGE_PROCESSING_JNI_PATH := $(LOCAL_PATH)/image_processing
+
+TESS_TWO_PATH := $(LOCAL_PATH)/../../../../tess-two/tess-two
+LEPTONICA_SRC_PATH := $(TESS_TWO_PATH)/jni/com_googlecode_leptonica_android/src
+
+LIBJPEG_PATH := $(LOCAL_PATH)/../../../../libjpeg
+LIBPNG_PATH := $(LOCAL_PATH)/../../../../libpng-android/jni
+
+
+include $(LIBJPEG_PATH)/Android.mk
+include $(LIBPNG_PATH)/Android.mk
+include $(HOCR2PDF_PATH)/Android.mk
+include $(HOCR2PDF_JNI_PATH)/Android.mk
+include $(IMAGE_PROCESSING_JNI_PATH)/Android.mk
--- textfairy/jni/leptonica/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := liblept
-LOCAL_SRC_FILES := $(TESS_TWO_PATH)/libs/$(TARGET_ARCH_ABI)/liblept.so
-LOCAL_EXPORT_C_INCLUDES := $(LEPTONICA_SRC_PATH)/src
-include $(PREBUILT_SHARED_LIBRARY)
-
