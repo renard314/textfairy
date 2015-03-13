0846485127fc5198d81016104b5c62ed72fd89d4
Renard Wellnitz <renard.wellnitz@googlemail.com>
2015-03-13 23:01:31 +0100

    update license

--- app/src/main/java/com/renard/ocr/cropimage/CropImageActivity.java
+++ app/src/main/java/com/renard/ocr/cropimage/CropImageActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
- * Copyright (C) 2012,2013 Renard Wellnitz
+ * Copyright (C) 2012,2013,2014,2015 Renard Wellnitz
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
