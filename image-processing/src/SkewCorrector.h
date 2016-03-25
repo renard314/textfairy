5e79d4675baf4a2e825477174c19d8c0d424c830
Renard Wellnitz <renard.wellnitz@googlemail.com>
2016-03-25 12:25:55 +0800

    chore: reformat

--- image-processing/src/SkewCorrector.h
+++ image-processing/src/SkewCorrector.h
@@ -7,16 +7,16 @@
 
 #ifndef SKEWCORRECTOR_H_
 #define SKEWCORRECTOR_H_
-#include <allheaders.h>
+#include "allheaders.h"
 
 class SkewCorrector {
 public:
 	SkewCorrector(bool debug);
 	Pix* correctSkew(Pix* pix, l_float32* angle);
-	Pix* rotate(Pix* pix, l_float32 angle);
 	virtual ~SkewCorrector();
 
 private:
+    Pix* rotate(Pix* pix, l_float32 angle);
 	static l_float32 const DEG_2_RAD;
 	bool mDebug;
 };
