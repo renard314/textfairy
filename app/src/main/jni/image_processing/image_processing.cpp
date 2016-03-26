33adc65d0c2a4e6f319dae1755fd3aa07f2bc8db
Renard Wellnitz <renard.wellnitz@googlemail.com>
2016-03-26 12:33:55 +0800

    feat: correct skew

--- app/src/main/jni/image_processing/image_processing.cpp
+++ app/src/main/jni/image_processing/image_processing.cpp
@@ -148,6 +148,13 @@ extern "C" {
         pixb = binarizer.binarize(pixOrg, pixJavaCallback);
         pixJavaCallback(pixb);
         
+        SkewCorrector skewCorrector(false);
+        Pix* pixbRotated = skewCorrector.correctSkew(pixb, NULL);
+        pixDestroy(&pixb);
+        pixb = pixbRotated;
+        
+        pixJavaCallback(pixb);
+        
         segmentComplexLayout(pixOrg, NULL, pixb, &pixaImages, &pixaTexts, callbackLayout, true);
         
         if (isStateValid()) {
