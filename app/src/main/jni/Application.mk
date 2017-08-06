cf2c1e958d0a16cfb24c1e35536dc180b6a8d59e
Renard Wellnitz <rwellnitz@zalando-25973.fritz.box>
2017-08-06 22:38:04 +0200

    feat: compile with android-ndk-r15c

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,6 +2,6 @@ ANDROID_JPEG_NO_ASSEMBLER := true
 APP_PLATFORM := android-9
 APP_STL := gnustl_static #gnustl_shared
 # ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi armeabi-v7a x86 
+APP_ABI := armeabi-v7a x86 
 APP_OPTIM := release
 #APP_CPPFLAGS += -frtti
