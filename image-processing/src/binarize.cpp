119be7616a3ede8f54df46d7ccd051fd64c70036
Renard Wellnitz <renard.wellnitz@googlemail.com>
2018-02-13 21:11:32 +0800

    fix: compiler error related to depreciation of GPLOT_X11

--- image-processing/src/binarize.cpp
+++ image-processing/src/binarize.cpp
@@ -81,7 +81,7 @@ int determineThresholdForTile(Pix* pixt, bool debug) {
 		GPLOT *gplot;
 		ostringstream name;
 		name << mean;
-		gplot = gplotCreate(name.str().c_str(), GPLOT_X11, name.str().c_str(),
+		gplot = gplotCreate(name.str().c_str(), GPLOT_PNG, name.str().c_str(),
 				"x", "y");
 		ostringstream title;
 		title << "mean = " << mean << ", " << "variance = " << variance
