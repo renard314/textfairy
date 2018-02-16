2ae6d07a09e17cb7bae18780666080f7f697f2bb
Renard Wellnitz <renard.wellnitz@googlemail.com>
2018-02-16 22:30:32 +0800

    fix: match ndk APP_PLATFORM with min sdk version

--- app/Application.mk
+++ app/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := c++_shared
 APP_ABI :=  armeabi-v7a x86
 APP_OPTIM := release
-APP_PLATFORM := android-9
+APP_PLATFORM := android-15
 APP_CPPFLAGS += -fexceptions -frtti
 NDK_TOOLCHAIN_VERSION := clang
