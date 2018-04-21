eecf4ba00104f7f7a29499c83c680f37052d5b26
Renard Wellnitz <renard.wellnitz@googlemail.com>
2018-04-21 22:54:10 +0200

    fix: compile error after updating support lib

--- app/src/main/java/com/renard/ocr/documents/creation/crop/BaseActivityInterface.java
+++ app/src/main/java/com/renard/ocr/documents/creation/crop/BaseActivityInterface.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2012,2013 Renard Wellnitz.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,12 +15,10 @@
  */
 package com.renard.ocr.documents.creation.crop;
 
-import android.view.View;
-
 public interface BaseActivityInterface {
-	
-	public void setDialogId(int dialogId);
-	public View findViewById(int viewId);
-	public void showDialog(int dialogId);
+
+    public void setDialogId(int dialogId);
+
+    public void showDialog(int dialogId);
 
 }
